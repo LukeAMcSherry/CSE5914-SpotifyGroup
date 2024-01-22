6aa21b6ac240e6b5a1e07fa25d9aa02869791d4e
Steven Egnaczyk <stevenegnaczyk@gmail.com>
2024-01-22 16:06:23 -0500

    AHHH

--- react-flask-app/src/App.js
+++ react-flask-app/src/App.js
@@ -1,22 +1,31 @@
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        Crazy? I was crazy once.
+        They put me in a room.
+        A rubber room.
+        A rubber room with rats.
+        They put me in a rubber room with rubber rats.
+        Rubber rats? I hate rubber rats.
+        They make me crazy.
+        Crazy? I was crazy once.
+        They put me in a room….
+
+        <p>The current time is {currentTime}.</p>
       </header>
     </div>
   );
