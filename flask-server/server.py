969fac1a5f930951972e7667520ba56188625ee2
LukeAMcSherry <lukeamcsherry@gmail.com>
2024-01-22 11:46:58 -0500

    basic website

---
+++ flask-server/server.py
@@ -0,0 +1,11 @@
+import time
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/time')
+def get_current_time():
+    return {'time': time.time()}
+
+if __name__ == "__main__":
+    app.run(debug=True)
