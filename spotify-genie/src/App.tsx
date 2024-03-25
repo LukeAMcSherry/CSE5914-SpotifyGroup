10f9dc72de92839c703a1d4100e4fa500c0759b7
LukeAMcSherry <lukeamcsherry@gmail.com>
2024-03-24 23:32:21 -0400

    Added new pages to frontend

--- spotify-genie/src/App.tsx
+++ spotify-genie/src/App.tsx
@@ -5,6 +5,10 @@ import Header from './components/Header';
 import Home from './views/Home';
 import Playlist from './views/Playlist';
 import Callback from './views/Callback';
+import Features from './views/Features';
+import About from './views/About';
+
+
 
 
 function App() {
@@ -16,6 +20,8 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/playlist' element={<Playlist />} />
         <Route path='/callback' element={<Callback />} />
+        <Route path='/features' element={<Features />} />
+        <Route path='/about' element={<About />} />
       </Routes>
     </BrowserRouter>
   );
